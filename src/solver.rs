acb568bd03899318791845bb2eb76dacab7d95dc
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-01-30 14:23:34 +0100

    Created Solution struct.

--- src/solver.rs
+++ src/solver.rs
@@ -2,7 +2,7 @@ mod types;
 
 #[cfg(test)]
 mod tests {
-    use super::types::{Operation, Operator};
+    use super::types::{Operation, Operator, Solution};
 
     #[test]
     fn test_invalid_division_creation_1() {
@@ -16,28 +16,58 @@ mod tests {
 
     #[test]
     fn test_add_operation_equality() {
-        assert_eq!(Operation::new(Operator::Add, 3, 5), Operation::new(Operator::Add, 3, 5));
-        assert_eq!(Operation::new(Operator::Add, 3, 5), Operation::new(Operator::Add, 5, 3));
-        assert_ne!(Operation::new(Operator::Add, 3, 5), Operation::new(Operator::Add, 13, 5));
+        assert_eq!(
+            Operation::new(Operator::Add, 3, 5),
+            Operation::new(Operator::Add, 3, 5)
+        );
+        assert_eq!(
+            Operation::new(Operator::Add, 3, 5),
+            Operation::new(Operator::Add, 5, 3)
+        );
+        assert_ne!(
+            Operation::new(Operator::Add, 3, 5),
+            Operation::new(Operator::Add, 13, 5)
+        );
     }
 
     #[test]
     fn test_sub_operation_equality() {
-        assert_eq!(Operation::new(Operator::Sub, 10, 8), Operation::new(Operator::Sub, 10, 8));
-        assert_ne!(Operation::new(Operator::Sub, 10, 8), Operation::new(Operator::Sub, 8, 10));
+        assert_eq!(
+            Operation::new(Operator::Sub, 10, 8),
+            Operation::new(Operator::Sub, 10, 8)
+        );
+        assert_ne!(
+            Operation::new(Operator::Sub, 10, 8),
+            Operation::new(Operator::Sub, 8, 10)
+        );
     }
 
     #[test]
     fn test_mul_operation_equality() {
-        assert_eq!(Operation::new(Operator::Mul, 3, 5), Operation::new(Operator::Mul, 3, 5));
-        assert_eq!(Operation::new(Operator::Mul, 3, 5), Operation::new(Operator::Mul, 5, 3));
-        assert_ne!(Operation::new(Operator::Mul, 3, 5), Operation::new(Operator::Mul, 13, 5));
+        assert_eq!(
+            Operation::new(Operator::Mul, 3, 5),
+            Operation::new(Operator::Mul, 3, 5)
+        );
+        assert_eq!(
+            Operation::new(Operator::Mul, 3, 5),
+            Operation::new(Operator::Mul, 5, 3)
+        );
+        assert_ne!(
+            Operation::new(Operator::Mul, 3, 5),
+            Operation::new(Operator::Mul, 13, 5)
+        );
     }
 
     #[test]
     fn test_div_operation_equality() {
-        assert_eq!(Operation::new(Operator::Div, 10, 2), Operation::new(Operator::Div, 10, 2));
-        assert_ne!(Operation::new(Operator::Div, 10, 2), Operation::new(Operator::Div, 5, 1));
+        assert_eq!(
+            Operation::new(Operator::Div, 10, 2),
+            Operation::new(Operator::Div, 10, 2)
+        );
+        assert_ne!(
+            Operation::new(Operator::Div, 10, 2),
+            Operation::new(Operator::Div, 5, 1)
+        );
     }
 
     #[test]
@@ -50,9 +80,97 @@ mod tests {
 
     #[test]
     fn test_operations_display() {
-        assert_eq!("3 + 2 = 5", format!("{}", Operation::new(Operator::Add, 3, 2).unwrap()));
-        assert_eq!("6 - 4 = 2", format!("{}", Operation::new(Operator::Sub, 6, 4).unwrap()));
-        assert_eq!("3 * 2 = 6", format!("{}", Operation::new(Operator::Mul, 3, 2).unwrap()));
-        assert_eq!("10 / 2 = 5", format!("{}", Operation::new(Operator::Div, 10, 2).unwrap()));
+        assert_eq!(
+            "3 + 2 = 5",
+            format!("{}", Operation::new(Operator::Add, 3, 2).unwrap())
+        );
+        assert_eq!(
+            "6 - 4 = 2",
+            format!("{}", Operation::new(Operator::Sub, 6, 4).unwrap())
+        );
+        assert_eq!(
+            "3 * 2 = 6",
+            format!("{}", Operation::new(Operator::Mul, 3, 2).unwrap())
+        );
+        assert_eq!(
+            "10 / 2 = 5",
+            format!("{}", Operation::new(Operator::Div, 10, 2).unwrap())
+        );
+    }
+
+    #[test]
+    fn test_solutions_equality() {
+        let solution_1_operations = vec![Operation::new(Operator::Add, 3, 2).unwrap()];
+        let solution_2_operations = vec![Operation::new(Operator::Add, 3, 2).unwrap()];
+        let solution_1 = Solution::new(solution_1_operations);
+        let solution_2 = Solution::new(solution_2_operations);
+        assert_eq!(solution_1, solution_2);
+
+        let solution_3_operations = vec![
+            Operation::new(Operator::Add, 3, 2).unwrap(),
+            Operation::new(Operator::Mul, 10, 6).unwrap(),
+        ];
+        let solution_4_operations = vec![
+            Operation::new(Operator::Mul, 10, 6).unwrap(),
+            Operation::new(Operator::Add, 3, 2).unwrap(),
+        ];
+        let solution_3 = Solution::new(solution_3_operations);
+        let solution_4 = Solution::new(solution_4_operations);
+        assert_eq!(solution_3, solution_4);
+
+        let solution_5_operations = vec![
+            Operation::new(Operator::Mul, 4, 5).unwrap(),
+            Operation::new(Operator::Add, 3, 2).unwrap(),
+            Operation::new(Operator::Mul, 10, 6).unwrap(),
+        ];
+        let solution_6_operations = vec![
+            Operation::new(Operator::Mul, 10, 6).unwrap(),
+            Operation::new(Operator::Mul, 4, 5).unwrap(),
+            Operation::new(Operator::Add, 3, 2).unwrap(),
+        ];
+        let solution_5 = Solution::new(solution_5_operations);
+        let solution_6 = Solution::new(solution_6_operations);
+        assert_eq!(solution_5, solution_6);
+
+        let solution_7_operations = vec![
+            Operation::new(Operator::Mul, 4, 5).unwrap(),
+            Operation::new(Operator::Add, 3, 2).unwrap(),
+            Operation::new(Operator::Mul, 10, 6).unwrap(),
+        ];
+        let solution_8_operations = vec![
+            Operation::new(Operator::Mul, 10, 6).unwrap(),
+            Operation::new(Operator::Mul, 4, 5).unwrap(),
+        ];
+        let solution_7 = Solution::new(solution_7_operations);
+        let solution_8 = Solution::new(solution_8_operations);
+        assert_ne!(solution_7, solution_8);
+
+        let solution_9_operations = vec![
+            Operation::new(Operator::Mul, 4, 5).unwrap(),
+            Operation::new(Operator::Add, 3, 2).unwrap(),
+            Operation::new(Operator::Mul, 10, 6).unwrap(),
+        ];
+        let solution_10_operations = vec![
+            Operation::new(Operator::Mul, 10, 6).unwrap(),
+            Operation::new(Operator::Mul, 4, 5).unwrap(),
+            Operation::new(Operator::Mul, 4, 5).unwrap(),
+        ];
+        let solution_9 = Solution::new(solution_9_operations);
+        let solution_10 = Solution::new(solution_10_operations);
+        assert_ne!(solution_9, solution_10);
+
+        let solution_11_operations = vec![
+            Operation::new(Operator::Mul, 10, 6).unwrap(),
+            Operation::new(Operator::Mul, 4, 5).unwrap(),
+            Operation::new(Operator::Mul, 4, 5).unwrap(),
+        ];
+        let solution_12_operations = vec![
+            Operation::new(Operator::Mul, 4, 5).unwrap(),
+            Operation::new(Operator::Add, 3, 2).unwrap(),
+            Operation::new(Operator::Mul, 10, 6).unwrap(),
+        ];
+        let solution_11 = Solution::new(solution_11_operations);
+        let solution_12 = Solution::new(solution_12_operations);
+        assert_ne!(solution_11, solution_12);
     }
 }
--- src/solver/types.rs
+++ src/solver/types.rs
@@ -1,7 +1,8 @@
+use std::clone::Clone;
 use std::cmp::PartialEq;
-use std::fmt::{Display, Formatter, Result};
+use std::fmt::{Debug, Display, Formatter, Result};
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum Operator {
     Add,
     Sub,
@@ -61,7 +62,14 @@ impl Operation {
 
 impl Display for Operation {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(f, "{} {} {} = {}", self.operand_1, self.operator, self.operand_2, self.value())
+        write!(
+            f,
+            "{} {} {} = {}",
+            self.operand_1,
+            self.operator,
+            self.operand_2,
+            self.value()
+        )
     }
 }
 
@@ -78,5 +86,94 @@ impl PartialEq for Operation {
                 _ => self.operand_1 == other.operand_1 && self.operand_2 == other.operand_2,
             }
         }
+    }
+}
+
+impl Clone for Operation {
+    fn clone(&self) -> Self {
+        Operation {
+            operand_1: self.operand_1,
+            operand_2: self.operand_2,
+            operator: self.operator.clone(),
+        }
+    }
+}
+
+pub struct Solution {
+    operations: Vec<Operation>,
+}
+
+impl Solution {
+    pub fn new(operations: Vec<Operation>) -> Self {
+        Solution {
+            operations: operations,
+        }
+    }
+}
+
+impl PartialEq for Solution {
+    fn eq(&self, other: &Self) -> bool {
+        if self.operations.len() != other.operations.len() {
+            false
+        } else {
+            let mut are_equals = true;
+            // each member of self must be on other
+            let self_operations_clone = self.operations.clone();
+            for this_member in self_operations_clone {
+                let found = other
+                    .operations
+                    .clone()
+                    .into_iter()
+                    .filter(|x| *x == this_member)
+                    .collect::<Vec<_>>()
+                    .len()
+                    > 0;
+                if !found {
+                    are_equals = false;
+                    break;
+                }
+            }
+            // each member of other must be in self
+            let other_operations_clone = other.operations.clone();
+            for other_member in other_operations_clone {
+                let found = self
+                    .operations
+                    .clone()
+                    .into_iter()
+                    .filter(|x| *x == other_member)
+                    .collect::<Vec<_>>()
+                    .len()
+                    > 0;
+                if !found {
+                    are_equals = false;
+                    break;
+                }
+            }
+
+            are_equals
+        }
+    }
+}
+
+impl Clone for Solution {
+    fn clone(&self) -> Self {
+        Solution {
+            operations: self.operations.clone(),
+        }
+    }
+}
+
+impl Debug for Solution {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(
+            f,
+            "Solution[{}]",
+            self.operations
+                .clone()
+                .into_iter()
+                .map(|curr| curr.to_string())
+                .collect::<Vec<_>>()
+                .join(", ")
+        )
     }
 }
