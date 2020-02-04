0ca644611a510a9f5469698ed99c7f3f5d9e6de2
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-02-04 19:29:26 +0100

    Replaced tiles components with comboBoxes

--- src/gui.rs
+++ src/gui.rs
@@ -1,7 +1,7 @@
 use crate::solver::{Solution};
 
-use gtk::{prelude::*, Inhibit, Orientation::Horizontal, Orientation::Vertical};
-use relm::{Widget, Component, init};
+use gtk::{prelude::*, Inhibit, Orientation::Horizontal, Orientation::Vertical, CellRendererText};
+use relm::{Relm, Widget, Component, init, connect};
 use relm_derive::{Msg, widget};
 
 #[derive(Msg)]
@@ -11,13 +11,45 @@ pub enum TilesMsg {
 
 pub struct TilesModel {
     tiles: [u32; 6],
+    relm: Relm<TilesComp>,
 }
 
 #[widget]
 impl Widget for TilesComp {
-    fn model() -> TilesModel {
+    fn init_view(&mut self) {
+        for i in 0..6 {
+            self.set_model_in_tile(i);
+        }
+    }
+
+    fn set_model_in_tile(&mut self, tile_component_index: isize) {
+        let list_store = gtk::ListStore::new(&[u32::static_type()]);
+        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 75, 100];
+
+        for current in values.iter() {
+            list_store.insert_with_values(None, &[0], &[current]);
+        }
+
+        let tile_component = match tile_component_index {
+            0 => &self.tile_0,
+            1 => &self.tile_1,
+            2 => &self.tile_2,
+            3 => &self.tile_3,
+            4 => &self.tile_4,
+            5 => &self.tile_5,
+            _ => panic!("Incorrect tile index")
+        };
+
+        tile_component.set_model(Some(&list_store));
+        let renderer = CellRendererText::new();
+        tile_component.pack_start(&renderer, true);
+        tile_component.add_attribute(&renderer, "text", 0);
+    }
+
+    fn model(relm: &Relm<Self>, _: ()) -> TilesModel {
         TilesModel {
             tiles: [0; 6],
+            relm: relm.clone(),
         }
     }
 
@@ -32,57 +64,23 @@ impl Widget for TilesComp {
             orientation: Horizontal,
             spacing: 4,
 
-            gtk::Entry {
-                max_length: 3,
-                max_width_chars: 3,
-                width_chars: 3,
-                placeholder_text: Some("000"),
-                editing_done => {
-                    
-                }
-            },
+            #[name="tile_0"]
+            gtk::ComboBox {},
 
-            gtk::Entry {
-                max_length: 3,
-                max_width_chars: 3,
-                width_chars: 3,
-                placeholder_text: Some("000"),
-            },
+            #[name="tile_1"]
+            gtk::ComboBox {},
 
-            gtk::Entry {
-                max_length: 3,
-                max_width_chars: 3,
-                width_chars: 3,
-                placeholder_text: Some("000"),
-            },
+            #[name="tile_2"]
+            gtk::ComboBox {},
 
-            gtk::Entry {
-                max_length: 3,
-                max_width_chars: 3,
-                width_chars: 3,
-                placeholder_text: Some("000"),
-            },
+            #[name="tile_3"]
+            gtk::ComboBox {},
 
-            gtk::Entry {
-                max_length: 3,
-                max_width_chars: 3,
-                width_chars: 3,
-                placeholder_text: Some("000"),
-            },
+            #[name="tile_4"]
+            gtk::ComboBox {},
 
-            gtk::Entry {
-                max_length: 3,
-                max_width_chars: 3,
-                width_chars: 3,
-                placeholder_text: Some("000"),
-            },
-
-            gtk::Entry {
-                max_length: 3,
-                max_width_chars: 3,
-                width_chars: 3,
-                placeholder_text: Some("000"),
-            }
+            #[name="tile_5"]
+            gtk::ComboBox {},
         }
     }
 }
